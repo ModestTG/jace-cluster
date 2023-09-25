9ec2bbf7d8bef9b20c574c05fe1abca0cf72a608
Elliot Weishaar <elliotweishaar27@gmail.com>
2023-09-25 12:44:36 -0500

    updated newapp script

--- kubernetes/hack/newapp.sh
+++ kubernetes/hack/newapp.sh
@@ -9,5 +9,29 @@
 
 # echo "$1 $2"
 mkdir -p ../apps/$1/$2/app
-touch ../apps/$1/$2/ks.yaml ../apps/$1/$2/app/kustomization.yaml ../apps/$1/$2/app/helmrelease.yaml
+touch ../apps/$1/$2/app/helmrelease.yaml
+cat <<EOF > ../apps/$1/$2/ks.yaml
+---
+apiVersion: kustomize.toolkit.fluxcd.io/v1
+kind: Kustomization
+metadata:
+  name: $1-$2
+  namespace: flux-system
+spec:
+  path: ./kubernetes/apps/$1/$2/app
+  prune: true
+  sourceRef:
+    kind: GitRepository
+    name: home-kubernetes
+  interval: 30m
+  retryInterval: 1m
+  timeout: 3m
+EOF
+cat <<EOF > ../apps/$1/$2/app/kustomization.yaml
+---
+apiVersion: kustomize.config.k8s.io/v1beta1
+kind: Kustomization
+resources:
+  - helmrelease.yaml
+EOF
 echo "  - ./$2/ks.yaml" >> ../apps/$1/kustomization.yaml
